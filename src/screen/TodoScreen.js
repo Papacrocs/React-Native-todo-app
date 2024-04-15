bf1620f692d2d72320505bd129a165b3eff09704
Papacrocs <stephensamuelchung16@gmail.com>
2024-04-15 14:35:03 +0100

    Added handleAddTodo function

--- src/screen/TodoScreen.js
+++ src/screen/TodoScreen.js
@@ -1,47 +1,97 @@
-import { FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+import { IconButton } from "react-native-paper";
 
-
-const dummyData = [{
+const dummyData = [
+  {
     id: "01",
-    title: "Wash car"
-},
-{
+    title: "Wash car",
+  },
+  {
     id: "02",
-    title: "Read your new book"
-},
-{
+    title: "Read your new book",
+  },
+  {
     id: "03",
-    title: "Watch a movie"
-}
-]
+    title: "Watch a movie",
+  },
+];
 const TodoScreen = () => {
-    const renderTodos = ({item, index}) =>{
-        return (
-            <View style={{borderBottomWidth:2, marginBottom:6, borderBottomRightRadius:9, borderColor:"#1e90ff", paddingHorizontal:6, paddingVertical:12}}>
-                <Text>{item.title}</Text>
-            </View>
-        );
-    };
+    // Init local states
+    const [todo, setTodo] = useState("")
+    const [todoList, setTodoList] = useState([])
+    // Handle add todo
+    const handleAddTodo = () =>{
+        // Structure of single todo item
+        // {
+        //     id:
+        //     title:
+        // }
+        setTodoList([...todoList, {id: Date.now().toString(), title:todo}])
+    }
+    // Render todo
+  const renderTodos = ({ item, index }) => {
     return (
-        <View style={{ marginTop: 60, marginHorizontal: 16, }}>
-            
-            <TextInput style={{ borderWidth: 2, borderColor: "#1e90ff", borderRadius: 6, paddingVertical: 15, paddingHorizontal: 16, }}
-                placeholder='Add a Task'
-            />
-            <TouchableOpacity style={{backgroundColor:"#000", borderRadius:6, paddingVertical:8, marginVertical:35, alignItems:"center"}}>
-                <Text style={{color:"#fff", fontWeight:"bold", fontSize: 20}}>
-                    Add
-                </Text>
-            </TouchableOpacity>
+      <View
+        style={{
+          borderBottomWidth: 2,
+          marginBottom: 6,
+          borderBottomRightRadius: 9,
+          borderColor: "#BDBABA",
+          paddingHorizontal: 6,
+          paddingVertical: 12,
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        <Text style={{flex:1}}>{item.title}</Text>
+        <IconButton icon={"pencil"} iconColor="#2BEA25" />
+        <IconButton icon={"trash-can"} iconColor="#E70707" />
+      </View>
+    );
+  };
+  return (
+    <View style={{ marginTop: 60, marginHorizontal: 16 }}>
+      <TextInput
+        style={{
+          borderWidth: 2,
+          borderColor: "#1e90ff",
+          borderRadius: 6,
+          paddingVertical: 15,
+          paddingHorizontal: 16,
+        }}
+        placeholder="Add a Task"
+        value={todo}
+        onChangeText={(userText) => setTodo(userText)}
+      />
+      <TouchableOpacity
+        style={{
+          backgroundColor: "#063B74",
+          borderRadius: 6,
+          paddingVertical: 8,
+          marginVertical: 35,
+          alignItems: "center",
+        }}
+        onPress={ ()=> handleAddTodo()}
+      >
+        <Text style={{ color: "#fff", fontWeight: "bold", fontSize: 20 }}>
+          Add Task
+        </Text>
+      </TouchableOpacity>
 
-            {/* Render todo list */}
-            <FlatList data={dummyData} renderItem={renderTodos} />
-        </View>
+      {/* Render todo list */}
+      <FlatList data={todoList} renderItem={renderTodos} />
+    </View>
+  );
+};
 
-    )
-}
+export default TodoScreen;
 
-export default TodoScreen
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
